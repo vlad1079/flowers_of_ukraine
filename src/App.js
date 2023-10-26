dccadd01e33fa3b53b7e57e4254ed8819a4a3cbd
vlad1010 <misha1079@ukr.net>
2023-10-26 11:56:18 +0300

    26_10_2023d

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/header/Header';
 import './Reset.css';
 import './App.css';
+import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
